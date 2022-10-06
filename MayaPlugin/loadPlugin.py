3ca5746036cb287a8bd586abe78641d111765bee
Olowerr <oliver.klingspor@hotmail.com>
2022-10-06 17:13:54 +0200

    update

---
+++ MayaPlugin/loadPlugin.py
@@ -0,0 +1,18 @@
+import sys
+import os
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 1234))
+
+# Adam
+#s.send(b'loadPlugin("Mister Bister Sister Adam /Maya22-Gameplay3D/MayaPlugin/build/x64/Debug/UD1447Project.mll")\n')
+#s.send(b'loadPlugin("Mister Bister Sister Adam /Maya22-Gameplay3D/MayaPlugin/build/x64/Debug/UD1447Project.mll")\n')
+#s.send(b'loadPlugin("Mister Bister Sister Adam /Maya22-Gameplay3D/MayaPlugin/build/x64/Debug/UD1447Project.mll")\n')
+
+# Oliv
+s.send(b'loadPlugin("C:/Users/Oliver/source/repos/Maya22-Gameplay3D/MayaPlugin/build/x64/Debug/UD1447Project.mll")\n')  # Home
+#s.send(b'loadPlugin("C:/Users/Olive/source/repos/Maya22-Gameplay3D/MayaPlugin/build/x64/Debug/UD1447Project.mll")\n')   # Laptop
+#s.send(b'loadPlugin("C:/Users/a/source/repos/Maya22-Gameplay3D/MayaPlugin/build/x64/Debug/UD1447Project.mll")\n')       # School
+
+s.close()
